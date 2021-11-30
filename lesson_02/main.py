807fc9fd713a4b1e8c1ef2fd1a4f382b8d1a1f30
Evgeny <Evgeny.Popov4@evraz.com>
2021-11-30 14:57:52 +0700

    lessons_02

--- lesson_02/main.py
+++ lesson_02/main.py
@@ -1,2 +1,5 @@
-#isinstance('abc',str)
-print('hello')
+list_of_strings = ['The',' ','quick',' ','brown',' ','fox',' ','jumps',' ','over',' ','the',' ','lazy',' ','dog']
+for elem in list_of_strings:#
+    print(elem)
+
+
