ef3e06c9ea318565b5cedbf371b8ff8cc30c6f04
Evgeny <Evgeny.Popov4@evraz.com>
2021-11-30 17:00:09 +0700

    lessons_02

--- lesson_02/main.py
+++ lesson_02/main.py
@@ -1,5 +1,14 @@
 list_of_strings = ['The',' ','quick',' ','brown',' ','fox',' ','jumps',' ','over',' ','the',' ','lazy',' ','dog']
-for elem in list_of_strings:#
+for elem in list_of_strings:
     print(elem)
+c=list(range(0,100))
+print(c)
+d=c;
+for i in d:
+    d = i ** 2
+    print(d)
+
+
+
 
 
