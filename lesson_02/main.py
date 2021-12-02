8649e13ddc7e0a2bff3c7e88852082f6da163d36
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-02 12:51:18 +0700

    lessons_02

--- lesson_02/main.py
+++ lesson_02/main.py
@@ -1,13 +1,37 @@
 list_of_strings = ['The',' ','quick',' ','brown',' ','fox',' ','jumps',' ','over',' ','the',' ','lazy',' ','dog']
 for elem in list_of_strings:
     print(elem)
-Type_list_numbers=list(range(0,100))
+
+
+Type_list_numbers = list(range(0,5))
 print(Type_list_numbers)
-Square_of_numbers=Type_list_numbers;
-for iterator in Square_of_numbers:
-    Square_of_numbers = iterator ** 2
-    print(Square_of_numbers)
-Numbers_dict = {}
+
+
+for iterator in Type_list_numbers:
+    Type_list_numbers = iterator ** 2
+    print(Type_list_numbers,end='')
+
+    dict={Type_list_numbers:Type_list_numbers}
+    print(dict,end='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
