683f1ea167ec6e36590bc2b6aed97ac7cccc84c1
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-02 16:21:05 +0700

    lessons_02

--- lesson_02/main.py
+++ lesson_02/main.py
@@ -7,9 +7,9 @@ Type_list_numbers = list(range(0,100))
 print(Type_list_numbers)
 
 
-for iterator in Type_list_numbers:
-    Type_list_numbers = iterator ** 2
-    dict = {Type_list_numbers: Type_list_numbers}
+for container in Type_list_numbers:
+    Type_list_numbers = container ** 2
+    dict = {0: Type_list_numbers,1:Type_list_numbers,2:Type_list_numbers}
     print(Type_list_numbers,dict)
 
 
