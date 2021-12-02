75268514958e103fca3a6f83caafc569194936eb
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-02 15:48:10 +0700

    lessons_02

--- lesson_02/main.py
+++ lesson_02/main.py
@@ -1,19 +1,17 @@
-from this import s
-
 list_of_strings = ['The',' ','quick',' ','brown',' ','fox',' ','jumps',' ','over',' ','the',' ','lazy',' ','dog']
 for elem in list_of_strings:
     print(elem)
 
 
-Type_list_numbers = list(range(0,5))
+Type_list_numbers = list(range(0,100))
 print(Type_list_numbers)
 
 
 for iterator in Type_list_numbers:
     Type_list_numbers = iterator ** 2
     dict = {Type_list_numbers: Type_list_numbers}
+    print(Type_list_numbers,dict)
 
-    print(Type_list_numbers,dict,end='   ')
 
 
 
