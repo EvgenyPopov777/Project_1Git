5bf1375d7960a7ea029a65f5171abf4edf83578b
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-03 13:49:57 +0700

    leassons_03

--- homework_03/main_3.py
+++ homework_03/main_3.py
@@ -1,14 +1,18 @@
-string_list = ['hello','world','apple','earch','world']#Уникальность так же и с выводом простых чисел
-set(string_list)
-list(set(string_list))
-known_strings =set()
-for string in string_list:
-     if string in known_strings:
-         continue
-     print('found a new string',string)
-     known_strings.add(string)
-power =0
-def show_numbers(*numbers,power=2):
+# string_list = ['hello','world','apple','earch','world']#Уникальность так же и с выводом простых чисел
+# set(string_list)
+# list(set(string_list))
+# known_strings =set()
+# for string in string_list:
+#      if string in known_strings:
+#          continue
+#      print('found a new string',string)
+#      known_strings.add(string)
+# power =0
+def show_numbers(*numbers):
     for num in numbers:
-        print(num**2)
-show_numbers(2,4,5,7,8)
+        if num % num == 0:
+            print(num)
+        else:
+         break
+show_numbers(1,2,3,10,12,15,0)
+
