63aba4571cb7da5fc4a1e8d7551e142cb76f774e
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-06 15:19:54 +0700

    leassons_04

---
+++ Homework_04/lessons_main.py
@@ -0,0 +1,9 @@
+def power_numbers(*numbers,cub=0):
+    for num in numbers:
+        if  num > 0:
+            # s = s+num
+            # k=k+1
+            print(num,end=' в кубе = ')
+            cub=num**3
+            print(cub)
+power_numbers(-3,1,2,3,4,5,0,6,-1)
