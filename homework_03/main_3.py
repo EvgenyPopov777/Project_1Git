a0756b45a9d081664300adf2f11726a672af0371
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-06 23:01:09 +0700

    workspace

--- homework_03/main_3.py
+++ homework_03/main_3.py
@@ -1,4 +1,21 @@
 #---------------------------------------------------Задание № 2
+def is_prime(num):
+    if num == 2 or num == 3: return True
+    if num % 2 == 0 or num < 2: return False
+    for i in range(3, int(num ** 0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+def fl(*numbers):
+    return list(filter(is_prime, numbers))
+    print(numbers)
+result =fl(2,3,4,5,6,7,8,9,10)
+print(result)
+
+
+
+
+#---------------------------------------------------Задание № 2
 def fl_ly (string_list):
     print(string_list)
     set(string_list)
@@ -10,4 +27,5 @@ def fl_ly (string_list):
         print(string)
         known_strings.add(string)
 fl_ly(['hello','hello','my','world'])
+
 
