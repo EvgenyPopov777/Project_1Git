70a6fcae287de24aefbc823a0119df92eebd5c82
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-08 19:58:53 +0700

    lesson_04

--- Homework_04/lessons_04_main.py
+++ Homework_04/lessons_04_main.py
@@ -1,11 +1,8 @@
-def power_numbers(*numbers,cub=0):
-    for num in numbers:
-        if  num > 0:
-            # s = s+num
-            # k=k+1
-            print(num,end=' в кубе = ')
-            cub=num**3
-            print(cub)
-power_numbers(-3,1,2,3,4,5,0,6,-1)
-
-
+#-----------------------Задание №1
+def power_numbers(*number):
+    u =[]
+    for i in number:
+        i=i**3
+        u.append(i)
+    return u
+print(power_numbers(1,2,3,4,5,6))
