144c6dd15f7fa62af5b31ece2244d1dd8a738123
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-09 17:16:27 +0700

    lessons_04

--- Homework_04/lessons_04_main.py
+++ Homework_04/lessons_04_main.py
@@ -1,11 +1,11 @@
-# #-----------------------Задание №1
-# def power_numbers(*number):
-#     u =[]
-#     for i in number:
-#         i=i**3
-#         u.append(i)
-#     return u
-# print(power_numbers(1,2,3,4,5,6))
+#-----------------------Задание №1
+def power_numbers(*number):
+    u =[]
+    for i in number:
+        i=i**3
+        u.append(i)
+    return u
+print(power_numbers(1,2,3,4,5,6))
 
 
 
