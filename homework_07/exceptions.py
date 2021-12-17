74424058e039457c5cd431cb13da97323634d3f1
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-17 19:20:56 +0700

    lessons_07

---
+++ homework_07/exceptions.py
@@ -0,0 +1,9 @@
+class Exceptions():
+    def get_room_number():
+        while True:
+            try:
+                num = int(input("Введите номер комнаты: "))
+                print(f"Комната {num} успешно забронирована!")
+            except ValueError:
+                print("Исключение: Вы ввели буквы, а нужно число. Повторите ввод:")
+
