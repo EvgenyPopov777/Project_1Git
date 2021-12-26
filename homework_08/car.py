566d3e67f1cb2044b18930730d801b7602d3bb9b
Evgeny <Evgeny.Popov4@evraz.com>
2021-12-26 19:27:08 +0700

    homework_08

--- homework_08/car.py
+++ homework_08/car.py
@@ -1,58 +1,53 @@
-from dataclasses import  dataclass
 from pydantic import BaseModel
-from exceptions import exceept_1,exceept_2
-@dataclass
+from exceptions import LowFuelError,ValueError
+# Родитель
 class VehicleBase(BaseModel):
-    color: str
+    color:str
 
-
-@dataclass
-class Car():# Единственное, что я не сделал, так это наследование от VehiclaBase у Car и Plane. С ним, вся конструкция рушится.
-    fuel: int
-    started: bool = False
-
-
-#Функция включения и выключения машины по топливу
+#Машина...дочерний
+class Car(VehicleBase):
+    fuel:int
+    started:bool
+#Функция включения и выключения машины по топливу....
     def start(self):
         if self.fuel > 0:
             self.started = True
-            print('Топливо > 0 и  = '+str(self.fuel)+' Включаем:',self.started)
+            print('Топливо > 0 и  = '+str(self.fuel)+' Включаем, self.started = ',self.started)
         else:
-            print(exceept_1.fing())
+            raise LowFuelError('Недостаточно топлива')
 
     def stop(self):
-            # self.fuel = 0
             self.started = False
             print('Выключаем. Значение = ',self.started)
-
-@dataclass
-class Plan():
+#Груз...дочеринй
+class Plan(VehicleBase):
     cargo: int # текущий груз
+    max_cargo: int  # максимальный груз
     additional_cargo: int  # добавочный груз
-    max_cargo: int # максимальный груз
-
-#Функция проверки груза
+#Функция проверки груза...
     def add_cargo(self):# должно быть так: если текущй груз + добавочный груз <= max.cargo(максимальный груз), то вывести результат иначе вывести исключение вывести исключение
         self.cargo =self.cargo + self.additional_cargo
         if self.cargo < self.max_cargo:
             print('Лимит веса = '+ str(self.max_cargo) + '. Сумма веса = ' + str(self.cargo) + ' и не привышает лимит.')
+            print('self.cargo = ',self.cargo)
         else:
-            print(exceept_2.fin())
-
-
+            raise ValueError('Превышен лимит груза')
+# Функция удаления груза...
     def remove_all_cargo(self):
-        num = self.cargo
+        # num = self.cargo
+        print('обнуляем self.cargo :')
         self.cargo = 0
-        return num
-
-
+        return self.cargo
+#Основная функция...
 def main():
-    car = Car(1)
+    car =Car(color = 'Red', fuel = 0, started = False)
+    print(car)
     car.start()
-    # car.stop()
-    plan = Plan(50,30,100)#указываем текущий вес, затем добавочный и максимальный вес!
+    plan =Plan(color = 'Red', cargo = 50, max_cargo = 200, additional_cargo=60,)
+    print(plan)
     plan.add_cargo()
     print(plan.remove_all_cargo())
-
-
+    plan.add_cargo()
 main()
+
+
